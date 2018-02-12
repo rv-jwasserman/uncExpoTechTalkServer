072d68021e5b3e174bc9c4aec06ded038fcecebd
Joel Wasserman <jwasserman@redventures.com>
2018-02-12 16:25:15 -0500

    added endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const incommingMessages = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/twilio', (req, res, next) => {
-  console.log(req);
+router.post('/twilio', (req, res, next) => {
+  console.log(req.body.Body)
+  incommingMessages.push({ text: req.body.Body, time: Date.now() })
+  res.send(200)
+})
+
+router.get('/getMessages/:latestTime?', (req, res) => {
+  if (!req.params.latestTime) return res.status(200).json({ message: incommingMessages, success: true })
+  const returnMessages = incommingMessages.filter(message => message.time > req.params.latestTime)
+  return res.status(200).json({ message: returnMessages, success: true })
 })
 
 module.exports = router;
